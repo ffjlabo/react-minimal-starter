bc342414d9423c98521eba110a49a9e3ef68530f
fujiwo <ffjlabo@gmail.com>
2020-02-16 17:53:05 +0900

    update: webpackでの環境を大幅に変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,40 @@
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const {DefinePlugin} = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv');
+
+// 環境変数
+env = dotenv.config()['parsed'];
+for (key in env) {
+  env[key] = JSON.stringify(env[key]);
+}
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader"
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new htmlWebpackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        })
-    ]
-}
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+  },
+  plugins: [
+    new DefinePlugin(env),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+  ],
+};
